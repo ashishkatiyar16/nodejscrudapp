744c8ec0282e7c77be10e2d4e4792230f7c37740
Ashish katiyar <vikas.cool@gmail.com>
2015-07-21 22:14:41 +0530

    There was some files was missing in loging module that again i have commted

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -8,18 +7,18 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
-//load customers route
-var customers = require('./routes/customers'); 
+// load customers route
+var customers = require('./routes/customers');
 var app = express();
 
-var connection  = require('express-myconnection'); 
+var connection = require('express-myconnection');
 var mysql = require('mysql');
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(express.favicon());
+// app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -29,41 +28,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 /*------------------------------------------
-    connection peer, register as middleware
-    type koneksi : single,pool and request 
--------------------------------------------*/
+ connection peer, register as middleware
+ type koneksi : single,pool and request 
+ -------------------------------------------*/
 
 app.use(
-    
-    connection(mysql,{
-        
-        host: 'localhost',
-        user: 'root',
-        password : 'root',
-        port : 3306, //port mysql
-        database:'nodejs'
 
-    },'pool') //or single
+connection(mysql, {
+
+	host : 'localhost',
+	user : 'root',
+	password : 'root',
+	port : 3306, // port mysql
+	database : 'nodejs'
+
+}, 'pool') // or single
 
 );
-
-
-
 app.get('/', routes.index);
+app.post('/customers/login', customers.login_customer);
 app.get('/customers', customers.list);
 app.get('/customers/add', customers.add);
 app.post('/customers/add', customers.save);
 app.get('/customers/delete/:id', customers.delete_customer);
 app.get('/customers/edit/:id', customers.edit);
-app.post('/customers/edit/:id',customers.save_edit);
-
+app.post('/customers/edit/:id', customers.save_edit);
 
 app.use(app.router);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
